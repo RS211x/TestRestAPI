aa00465c6e04b1b6c27d012e5552d50e0a7c0814
Rohan Shrestha <Rohan.shrestha7148@gmail.com>
2025-06-03 16:53:58 +0100

    added features that create a sqlite db and allows for it to be updated with the new generated weather

--- DemoRestAPI/Program.cs
+++ DemoRestAPI/Program.cs
@@ -1,4 +1,17 @@
 using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+using System.Diagnostics.Eventing.Reader;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,9 +35,22 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hotter than hellfire", "Infernal", "Lava-like", "Boiling"
 };
 
+var sql = @"CREATE TABLE IF NOT EXISTS WeatherForecast (
+    Date DATE NOT NULL,
+    TemperatureC INT NOT NULL,
+    Summary NVARCHAR(100) NOT NULL,
+    TemperatureF INT NOT NULl
+);";
+
+using var connection = new SqliteConnection(@"Data Source=/Users/rohanshrestha/C#/WEX/WeatherForecast.db");
+connection.Open();
+using var command = new SqliteCommand(sql, connection);
+command.ExecuteNonQuery();
+Console.WriteLine("Table created successfully.");
+
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -33,9 +59,30 @@ app.MapGet("/weatherforecast", () =>
         ))
         .ToArray();
     return forecast;
+
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+List<WeatherEntry> weatherData = await FetchWeatherDataAsync();
+
+
+foreach (var entry in WeatherData.WeatherForecasts)
+{
+    var cmd = connection.CreateCommand();
+    cmd.CommandText =
+    @"INSERT INTO WeatherForecast (Date, Temperature, Condition)
+      VALUES ($date, $temp, $cond)
+      ON CONFLICT(Date) DO UPDATE SET
+      Temperature = excluded.Temperature,
+      Condition = excluded.Condition;
+                ";
+    cmd.Parameters.AddWithValue("$date", entry.Date);
+    cmd.Parameters.AddWithValue("$temp", entry.Temperature);
+    cmd.Parameters.AddWithValue("$cond", entry.Condition);
+
+}
+
 
 app.Run();
 
@@ -45,10 +92,6 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 }
 
 
-Console.WriteLine("Enter currency code (e.g., USD, EUR):");
-var currency = Console.ReadLine();
 
-Console.WriteLine("Enter quantity:");
-var quantity = Console.ReadLine();
 
 
